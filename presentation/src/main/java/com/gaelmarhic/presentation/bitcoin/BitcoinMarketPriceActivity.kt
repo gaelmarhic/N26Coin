5bd0bbc327cadb59fa9fad7db3923e95598748aa
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-04 20:07:06 +0100

    Moving the BitcoinMarketPriceActivity to the Presentation module.

--- presentation/src/main/java/com/gaelmarhic/presentation/bitcoin/BitcoinMarketPriceActivity.kt
+++ presentation/src/main/java/com/gaelmarhic/presentation/bitcoin/BitcoinMarketPriceActivity.kt
@@ -1,7 +1,8 @@
-package com.gaelmarhic.n26coin
+package com.gaelmarhic.presentation.bitcoin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gaelmarhic.presentation.R
 
 class BitcoinMarketPriceActivity : AppCompatActivity() {
 
