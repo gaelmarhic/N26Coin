e553c13ea54352a59c81a373667ab731f2a6d783
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-04 17:05:10 +0100

    Creating the project.

---
+++ app/src/main/java/com/gaelmarhic/n26coin/BitcoinMarketPriceActivity.kt
@@ -0,0 +1,12 @@
+package com.gaelmarhic.n26coin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BitcoinMarketPriceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bitcoin_market_price)
+    }
+}
