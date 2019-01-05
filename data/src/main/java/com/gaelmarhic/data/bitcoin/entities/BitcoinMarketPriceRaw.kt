faf104ab50bf512bc122d6b6dd64319485c698fb
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 11:18:20 +0100

    Creating the raw models for the Blockchain's market price chart API.

---
+++ data/src/main/java/com/gaelmarhic/data/bitcoin/entities/BitcoinMarketPriceRaw.kt
@@ -0,0 +1,18 @@
+package com.gaelmarhic.data.bitcoin.entities
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Gaël Marhic on 05/01/2019.
+ *
+ * @property timestamp The exact timestamp corresponding to this [BitcoinMarketPriceRaw] entry.
+ * @property amount The bitcoin's value corresponding to the given [timestamp].
+ */
+data class BitcoinMarketPriceRaw(
+
+        @SerializedName("x")
+        private val timestamp: Long?,
+
+        @SerializedName("y")
+        private val amount: Float?
+)
