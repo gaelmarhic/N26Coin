a2cf3492b66e7cdb3793aeec332d2ff52818b095
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 21:22:23 +0100

    Adding some classes extracted directly from https://github.com/n26/N26AndroidSamples to the domain module.

---
+++ domain/src/main/java/n26/README.md
@@ -0,0 +1,2 @@
+This package contains classes extracted directly from https://github.com/n26/N26AndroidSamples.
+The reason was simply because there was no necessity for adapting them. :)
---
+++ domain/src/main/java/n26/rx/UnwrapOptionTransformer.java
@@ -0,0 +1,23 @@
+package n26.rx;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import polanski.option.Option;
+import polanski.option.OptionUnsafe;
+
+/**
+ Filters out all Option of NONE if any, but if Some, then unwraps and returns the value.
+ */
+public final class UnwrapOptionTransformer<T> implements ObservableTransformer<Option<T>, T> {
+
+    @Override
+    public ObservableSource<T> apply(final Observable<Option<T>> upstream) {
+        return upstream.filter(Option::isSome)
+                       .map(OptionUnsafe::getUnsafe);
+    }
+
+    public static <T> UnwrapOptionTransformer<T> create() {
+        return new UnwrapOptionTransformer<>();
+    }
+}
