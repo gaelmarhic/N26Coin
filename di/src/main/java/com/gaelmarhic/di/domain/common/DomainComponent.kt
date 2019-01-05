134e30a7e19b5ab87f3860e1c8c1ac43bf17209b
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 20:38:03 +0100

    Creating BitcoinDomainModule.

--- di/src/main/java/com/gaelmarhic/di/domain/common/DomainComponent.kt
+++ di/src/main/java/com/gaelmarhic/di/domain/common/DomainComponent.kt
@@ -1,9 +1,10 @@
 package com.gaelmarhic.di.domain.common
 
+import com.gaelmarhic.di.domain.features.bitcoin.BitcoinDomainModule
 import dagger.Subcomponent
 
 /**
  * Created by Gaël Marhic on 05/01/2019.
  */
-@Subcomponent
+@Subcomponent(modules = [(BitcoinDomainModule::class)])
 interface DomainComponent
---
+++ di/src/main/java/com/gaelmarhic/di/domain/features/bitcoin/BitcoinDomainModule.kt
@@ -0,0 +1,10 @@
+package com.gaelmarhic.di.domain.features.bitcoin
+
+import dagger.Module
+
+/**
+ * Created by Gaël Marhic on 05/01/2019.
+ */
+@Module
+class BitcoinDomainModule {
+}
