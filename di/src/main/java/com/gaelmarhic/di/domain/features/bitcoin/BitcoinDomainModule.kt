a1e2e488e9382e26941331f6ce68f015032b1fd5
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 20:42:53 +0100

    Making BitcoinDomainModule provide BitcoinRepository.

--- di/src/main/java/com/gaelmarhic/di/domain/features/bitcoin/BitcoinDomainModule.kt
+++ di/src/main/java/com/gaelmarhic/di/domain/features/bitcoin/BitcoinDomainModule.kt
@@ -1,10 +1,25 @@
 package com.gaelmarhic.di.domain.features.bitcoin
 
+import com.gaelmarhic.data.common.store.ReactiveStore
+import com.gaelmarhic.data.features.bitcoin.mappers.BitcoinMarketPriceInformationMapper
+import com.gaelmarhic.data.features.bitcoin.network.BitcoinService
+import com.gaelmarhic.data.features.bitcoin.repositories.BitcoinRepositoryImpl
+import com.gaelmarhic.domain.features.bitcoin.entities.BitcoinMarketPriceInformation
+import com.gaelmarhic.domain.features.bitcoin.repositories.BitcoinRepository
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 /**
  * Created by Gaël Marhic on 05/01/2019.
  */
 @Module
 class BitcoinDomainModule {
+
+    @Provides
+    @Singleton
+    fun provideBitcoinRepository(store: ReactiveStore<BitcoinMarketPriceInformation>,
+                                 bitcoinService: BitcoinService,
+                                 mapper: BitcoinMarketPriceInformationMapper): BitcoinRepository =
+            BitcoinRepositoryImpl(store, bitcoinService, mapper)
 }
