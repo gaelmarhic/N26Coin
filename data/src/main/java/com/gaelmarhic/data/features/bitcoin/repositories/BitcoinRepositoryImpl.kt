a469a12e5a055000930d4766096493ae1b61cb6d
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 20:26:27 +0100

    Creating the BitcoinRepository as well as its implementation.

---
+++ data/src/main/java/com/gaelmarhic/data/features/bitcoin/repositories/BitcoinRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.gaelmarhic.data.features.bitcoin.repositories
+
+import com.gaelmarhic.data.common.store.ReactiveStore
+import com.gaelmarhic.data.features.bitcoin.mappers.BitcoinMarketPriceInformationMapper
+import com.gaelmarhic.data.features.bitcoin.network.BitcoinService
+import com.gaelmarhic.domain.features.bitcoin.entities.BitcoinMarketPriceInformation
+import com.gaelmarhic.domain.features.bitcoin.repositories.BitcoinRepository
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by Gaël Marhic on 05/01/2019.
+ *
+ * This class corresponds to the implementation of the [BitcoinRepository]. Its definition can be
+ * found in the domain module. It has been done this way in order to respect the Dependency
+ * Inversion principle from SOLID.
+ */
+class BitcoinRepositoryImpl(private val store: ReactiveStore<BitcoinMarketPriceInformation>,
+                            private val bitcoinService: BitcoinService,
+                            private val mapper: BitcoinMarketPriceInformationMapper): BitcoinRepository {
+
+    /**
+     * Function used to fetch the Bitcoin's market price information from the Blockchain's API.
+     *
+     * @return Returns a [Completable].
+     */
+    override fun fetchBitcoinMarketPriceInformation(): Completable =
+            bitcoinService.fetchBitcoinMarketPrice()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(Schedulers.computation())
+                    .map(mapper)
+                    .doOnSuccess(store::replace)
+                    .ignoreElement()
+
+    /**
+     * Function used to get the stream of the Bitcoin's market price information.
+     *
+     * @return Returns an [Observable].
+     */
+    override fun getBitcoinMarketPriceInformation() = store.get()
+}
---
+++ domain/src/main/java/com/gaelmarhic/domain/features/bitcoin/repositories/BitcoinRepository.kt
@@ -0,0 +1,30 @@
+package com.gaelmarhic.domain.features.bitcoin.repositories
+
+import com.gaelmarhic.domain.features.bitcoin.entities.BitcoinMarketPriceInformation
+import io.reactivex.Completable
+import io.reactivex.Observable
+import polanski.option.Option
+
+/**
+ * Created by Gaël Marhic on 05/01/2019.
+ *
+ * This interface corresponds to the definition of the [BitcoinRepository]. Its implementation
+ * can be found in the data module. It has been done this way in order to respect the Dependency
+ * Inversion principle from SOLID.
+ */
+interface BitcoinRepository {
+
+    /**
+     * Function used to fetch the Bitcoin's market price information from the Blockchain's API.
+     *
+     * @return Returns a [Completable].
+     */
+    fun fetchBitcoinMarketPriceInformation(): Completable
+
+    /**
+     * Function used to get the stream of the Bitcoin's market price information.
+     *
+     * @return Returns an [Observable].
+     */
+    fun getBitcoinMarketPriceInformation(): Observable<Option<BitcoinMarketPriceInformation>>
+}
