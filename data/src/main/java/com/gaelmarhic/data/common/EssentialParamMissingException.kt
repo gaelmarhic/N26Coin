41f0df91e178fd6ae2c110afd63313e1080a46f0
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 12:02:06 +0100

    Adding an exception that will be thrown whenever an essential param is not returned by the backend.

---
+++ data/src/main/java/com/gaelmarhic/data/common/EssentialParamMissingException.kt
@@ -0,0 +1,13 @@
+package com.gaelmarhic.data.common
+
+import java.lang.RuntimeException
+
+/**
+ * Created by Gaël Marhic on 05/01/2019.
+ *
+ * Exception thrown when an essential parameter is missing in the backend/network response.
+ * THANK YOU GUYS FOR THE INSPIRATION :)
+ */
+class EssentialParamMissingException(missingParams: String,
+                                     rawObject: Any):
+        RuntimeException("The params: $missingParams are missing in received object: $rawObject")
