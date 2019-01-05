e66f71482fd459ee48d5670dc244c9744cad2429
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 11:23:06 +0100

    Implementing the call to Blockchain's market price chart API in BitcoinService.

--- data/src/main/java/com/gaelmarhic/data/bitcoin/network/BitcoinService.kt
+++ data/src/main/java/com/gaelmarhic/data/bitcoin/network/BitcoinService.kt
@@ -1,6 +1,17 @@
 package com.gaelmarhic.data.bitcoin.network
 
+import com.gaelmarhic.data.bitcoin.entities.BitcoinMarketPriceInformationRaw
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * Created by Gaël Marhic on 05/01/2019.
  */
-interface BitcoinService
+interface BitcoinService {
+
+    @GET("charts/market-price")
+    fun fetchBitcoinMarketPrice(@Query("timespan") timeSpan: String? = null,
+                                @Query("start") start: String? = null):
+            Single<BitcoinMarketPriceInformationRaw>
+}
