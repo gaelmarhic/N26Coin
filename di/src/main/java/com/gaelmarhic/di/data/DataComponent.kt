138c0f09768b3bf44e6227b5f202f4a9aa6166ba
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 03:08:38 +0100

    Creating NetworkModule to inject network related dependencies.

--- di/src/main/java/com/gaelmarhic/di/data/DataComponent.kt
+++ di/src/main/java/com/gaelmarhic/di/data/DataComponent.kt
@@ -5,5 +5,5 @@ import dagger.Subcomponent
 /**
  * Created by Gaël Marhic on 05/01/2019.
  */
-@Subcomponent
+@Subcomponent(modules = [(NetworkModule::class)])
 interface DataComponent
---
+++ di/src/main/java/com/gaelmarhic/di/data/NetworkModule.kt
@@ -0,0 +1,9 @@
+package com.gaelmarhic.di.data
+
+import dagger.Module
+
+/**
+ * Created by Gaël Marhic on 05/01/2019.
+ */
+@Module
+class NetworkModule
