e5ee4b1fce5e0c8bf14e11de4a58a28de9168055
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 03:30:16 +0100

    Providing Blockchain's base url in NetworkModule.

---
+++ data/src/main/java/com/gaelmarhic/data/bitcoin/constants/Constants.kt
@@ -0,0 +1,12 @@
+package com.gaelmarhic.data.bitcoin.constants
+
+/**
+ * Created by Gaël Marhic on 05/01/2019.
+ */
+class Constants {
+
+    companion object {
+
+        const val BLOCKCHAIN_BASE_URL = "https://api.blockchain.info"
+    }
+}
