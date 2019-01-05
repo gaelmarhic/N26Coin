539817ecca276cdf4b8e7480a9441f34e1d55c09
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-05 18:14:30 +0100

    Adding the @Module annotation to the ProviderModule, since it had been forgotten before.

--- di/src/main/java/com/gaelmarhic/di/data/common/ProviderModule.kt
+++ di/src/main/java/com/gaelmarhic/di/data/common/ProviderModule.kt
@@ -1,12 +1,14 @@
 package com.gaelmarhic.di.data.common
 
 import com.gaelmarhic.data.common.providers.TimestampProvider
+import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 /**
  * Created by Gaël Marhic on 05/01/2019.
  */
+@Module
 class ProviderModule {
 
     @Provides
