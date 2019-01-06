a671f2c135672e3b724ed9304c45f7bdae12f8fe
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-06 15:53:16 +0100

    Injecting BitcoinMarketPriceChartViewModelFactory in BitcoinMarketPriceActivity.

--- presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/activities/BitcoinMarketPriceActivity.kt
+++ presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/activities/BitcoinMarketPriceActivity.kt
@@ -1,9 +1,18 @@
 package com.gaelmarhic.presentation.features.bitcoin.activities
 
 import com.gaelmarhic.presentation.R
-import com.gaelmarhic.presentation.base.BaseActivity
+import com.gaelmarhic.presentation.base.BaseInjectingActivity
+import com.gaelmarhic.presentation.features.bitcoin.viewmodels.BitcoinMarketPriceChartViewModelFactory
+import android.arch.lifecycle.ViewModel
+import javax.inject.Inject
 
-class BitcoinMarketPriceActivity: BaseActivity() {
+class BitcoinMarketPriceActivity: BaseInjectingActivity() {
+
+    /**
+     * ViewModel factory that we will use to instantiate our [ViewModel].
+     */
+    @Inject
+    lateinit var bitcoinMarketPriceChartViewModelFactory: BitcoinMarketPriceChartViewModelFactory
 
     override fun getLayoutId(): Int = R.layout.activity_bitcoin_market_price
 }
--- presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/viewmodels/BitcoinMarketPriceChartViewModelFactory.kt
+++ presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/viewmodels/BitcoinMarketPriceChartViewModelFactory.kt
@@ -2,12 +2,13 @@ package com.gaelmarhic.presentation.features.bitcoin.viewmodels
 
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import javax.inject.Inject
 
 /**
  * Created by Gaël Marhic on 06/01/2019.
  */
 @Suppress("UNCHECKED_CAST")
-class BitcoinMarketPriceChartViewModelFactory: ViewModelProvider.NewInstanceFactory() {
+class BitcoinMarketPriceChartViewModelFactory @Inject constructor(): ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return BitcoinMarketPriceChartViewModel() as T
