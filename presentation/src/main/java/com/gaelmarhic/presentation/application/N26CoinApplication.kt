882e49e575858eb814589075a172f3e6271aae77
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-06 13:25:29 +0100

    Implementing N26CoinApplication.

--- presentation/src/main/java/com/gaelmarhic/presentation/application/N26CoinApplication.kt
+++ presentation/src/main/java/com/gaelmarhic/presentation/application/N26CoinApplication.kt
@@ -1,8 +1,28 @@
 package com.gaelmarhic.presentation.application
 
+import android.app.Activity
 import android.app.Application
+import com.gaelmarhic.presentation.di.DaggerApplicationComponent
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
 /**
  * Created by Gaël Marhic on 05/01/2019.
  */
-class N26CoinApplication: Application()
+class N26CoinApplication: Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DaggerApplicationComponent.builder()
+                .application(this)
+                .build()
+                .inject(this)
+    }
+
+    override fun activityInjector() = activityDispatchingAndroidInjector
+}
