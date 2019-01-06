2fe55d126fae4e8371a7e1238cd75b22c7f2fbcf
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-06 14:07:02 +0100

    Creating BitcoinMarketPriceChartViewModel and its factory.

---
+++ presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/viewmodels/BitcoinMarketPriceChartViewModel.kt
@@ -0,0 +1,6 @@
+package com.gaelmarhic.presentation.features.bitcoin.viewmodels
+
+/**
+ * Created by Gaël Marhic on 06/01/2019.
+ */
+class BitcoinMarketPriceChartViewModel
---
+++ presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/viewmodels/BitcoinMarketPriceChartViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.gaelmarhic.presentation.features.bitcoin.viewmodels
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+
+/**
+ * Created by Gaël Marhic on 06/01/2019.
+ */
+@Suppress("UNCHECKED_CAST")
+class BitcoinMarketPriceChartViewModelFactory: ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return BitcoinMarketPriceChartViewModel() as T
+    }
+}
