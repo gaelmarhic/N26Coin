3e6a3bc4f9313ed6c88eb25414c4258a65b3dcf0
Gaël Marhic <gael.marhic.pro@gmail.com>
2019-01-06 19:37:36 +0100

    Setting up the ViewModel's LiveData observation in BitcoinMarketPriceActivity.

--- presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/activities/BitcoinMarketPriceActivity.kt
+++ presentation/src/main/java/com/gaelmarhic/presentation/features/bitcoin/activities/BitcoinMarketPriceActivity.kt
@@ -1,9 +1,15 @@
 package com.gaelmarhic.presentation.features.bitcoin.activities
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
 import com.gaelmarhic.presentation.R
 import com.gaelmarhic.presentation.base.BaseInjectingActivity
 import com.gaelmarhic.presentation.features.bitcoin.viewmodels.BitcoinMarketPriceChartViewModelFactory
 import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import com.gaelmarhic.presentation.features.bitcoin.entities.BitcoinMarketPriceInformationChartViewEntity
+import com.gaelmarhic.presentation.features.bitcoin.viewmodels.BitcoinMarketPriceChartViewModel
 import javax.inject.Inject
 
 class BitcoinMarketPriceActivity: BaseInjectingActivity() {
@@ -14,5 +20,29 @@ class BitcoinMarketPriceActivity: BaseInjectingActivity() {
     @Inject
     lateinit var bitcoinMarketPriceChartViewModelFactory: BitcoinMarketPriceChartViewModelFactory
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        observeViewModelLiveData()
+    }
+
     override fun getLayoutId(): Int = R.layout.activity_bitcoin_market_price
+
+    /**
+     * Function that will set up the [ViewModel]'s [LiveData] observation.
+     */
+    private fun observeViewModelLiveData() {
+        val viewModel = ViewModelProviders
+                .of(this, bitcoinMarketPriceChartViewModelFactory)
+                .get(BitcoinMarketPriceChartViewModel::class.java)
+
+        viewModel.bitcoinMarketPriceInformationLiveData.observe(
+                this, Observer { updateScreen(it) })
+    }
+
+    /**
+     * Function in charge of painting the screen according to [BitcoinMarketPriceInformationChartViewEntity].
+     */
+    private fun updateScreen(viewEntity: BitcoinMarketPriceInformationChartViewEntity?) {
+        // TODO: To be implemented.
+    }
 }
